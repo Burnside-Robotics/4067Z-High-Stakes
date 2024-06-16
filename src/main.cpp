3cdf7bee6515539c36b812b246c850724419a3a2
supereason333 <supereason333@gmail.com>
2024-06-16 14:49:31 +1200

    Working tank drive code

--- src/main.cpp
+++ src/main.cpp
@@ -75,8 +75,11 @@ void autonomous() {}
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+	//pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
+	//pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+
+	pros::MotorGroup left_mg({2, 11});
+	pros::MotorGroup right_mg({-10, -20});
 
 
 	while (true) {
@@ -85,10 +88,19 @@ void opcontrol() {
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
 
 		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
+		//int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
+		//int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
+		//left_mg.move(dir - turn);                      // Sets left motor voltage
+		//right_mg.move(dir + turn);                     // Sets right motor voltage
+		//pros::delay(20);                               // Run for 20 ms then update
+
+		int left = master.get_analog(ANALOG_LEFT_Y);
+		int right = master.get_analog(ANALOG_RIGHT_Y);
+
+		left_mg.move(left);
+		right_mg.move(right);
+
+		pros::lcd::set_text(3, "Left: " + std::to_string(left) + " Right: " + std::to_string(right));
+		pros::delay(20);
 	}
 }
